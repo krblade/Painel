670755b5775d73c544f62c55aef4d1f2f61e211b
BP6G <marcelo-ferreira.stefanini@petrobras.com.br>
2021-04-27 14:44:18 -0300

    Alteações no layout da tabela

--- paginas/urls.py
+++ paginas/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, TableView
 
 urlpatterns = [
     path('inicio/', IndexView.as_view(), name='inicio'),
+    path('tabela/', TableView.as_view(), name='tabela'),
     path('', IndexView.as_view(), name='inicio'),    
 ]
--- paginas/views.py
+++ paginas/views.py
@@ -3,3 +3,6 @@ from django.views.generic import TemplateView
 # Create your views here.
 class IndexView(TemplateView):
     template_name = "index.html"
+
+class TableView(TemplateView):
+    template_name = "table.html"
