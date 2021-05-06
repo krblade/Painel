7b13e0a40b45cf3cb3e5bebc4b33caac52c26bc7
BP6G <marcelo-ferreira.stefanini@petrobras.com.br>
2021-05-06 16:26:38 -0300

    Conexão com banco de dados mysql e testes

--- PainelSAM/settings.py
+++ PainelSAM/settings.py
@@ -75,10 +75,21 @@ WSGI_APPLICATION = 'PainelSAM.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'teste',
+        'USER': 'DB_GOPI_SAM',
+        'PASSWORD': 'sam_GOPI@2021',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
@@ -117,9 +128,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
